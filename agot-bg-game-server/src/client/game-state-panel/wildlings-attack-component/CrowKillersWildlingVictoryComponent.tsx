73f52f3ea583cdcc593be2c318ebec7e733151fb
gereon77 <gereon77@users.noreply.github.com>
2026-09-17 17:18:30 +0200

    Prettify

--- agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/CrowKillersWildlingVictoryComponent.tsx
+++ agot-bg-game-server/src/client/game-state-panel/wildlings-attack-component/CrowKillersWildlingVictoryComponent.tsx
@@ -1,38 +1,46 @@
-import {observer} from "mobx-react";
-import {Component, ReactNode} from "react";
+import { observer } from "mobx-react";
+import { Component, ReactNode } from "react";
 import GameStateComponentProps from "../GameStateComponentProps";
 import renderChildGameState from "../../utils/renderChildGameState";
 import SelectUnitsGameState from "../../../common/ingame-game-state/select-units-game-state/SelectUnitsGameState";
 import SelectUnitsComponent from "../SelectUnitsComponent";
-import CrowKillersWildlingVictoryGameState, { CrowKillersStep }
-    from "../../../common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/crow-killers-wildling-victory-game-state/CrowKillersWildlingVictoryGameState";
+import CrowKillersWildlingVictoryGameState, {
+  CrowKillersStep
+} from "../../../common/ingame-game-state/westeros-game-state/wildlings-attack-game-state/crow-killers-wildling-victory-game-state/CrowKillersWildlingVictoryGameState";
 import Col from "react-bootstrap/Col";
 import React from "react";
 import TakeControlOfEnemyPortGameState from "../../../common/ingame-game-state/take-control-of-enemy-port-game-state/TakeControlOfEnemyPortGameState";
 import TakeControlOfEnemyPortComponent from "../TakeControlOfEnemyPortComponent";
 
 @observer
-export default class CrowKillersWildlingVictoryComponent extends Component<GameStateComponentProps<CrowKillersWildlingVictoryGameState>> {
-    render(): ReactNode {
-        return (
-            <>
-                {this.props.gameState.childGameState instanceof SelectUnitsGameState &&
-                (this.props.gameState.step == CrowKillersStep.DEGRADING_KNIGHTS
-                    ? <Col xs={12}>
-                        <b>{this.props.gameState.childGameState.house.name}</b> replaces {this.props.gameState.childGameState.count} of their Knights with
-                        available Footmen.
-                    </Col>
-                    : this.props.gameState.step == CrowKillersStep.DESTROYING_KNIGHTS
-                        ? <Col xs={12}>
-                            <b>{this.props.gameState.childGameState.house.name}</b> has to destroy {this.props.gameState.childGameState.count} of their Knights before
-                            the rest can be replaced with Footmen.
-                        </Col>
-                        : <Col xs={12}>Invalid CrowKillersStep!</Col>)}
-                {renderChildGameState(this.props, [
-                    [SelectUnitsGameState, SelectUnitsComponent],
-                    [TakeControlOfEnemyPortGameState, TakeControlOfEnemyPortComponent]
-                ])}
-            </>
-        );
-    }
+export default class CrowKillersWildlingVictoryComponent extends Component<
+  GameStateComponentProps<CrowKillersWildlingVictoryGameState>
+> {
+  render(): ReactNode {
+    return (
+      <>
+        {this.props.gameState.childGameState instanceof SelectUnitsGameState &&
+          (this.props.gameState.step == CrowKillersStep.DEGRADING_KNIGHTS ? (
+            <Col xs={12}>
+              <b>{this.props.gameState.childGameState.house.name}</b> replaces{" "}
+              {this.props.gameState.childGameState.count} of their Knights with
+              available Footmen.
+            </Col>
+          ) : this.props.gameState.step ==
+            CrowKillersStep.DESTROYING_KNIGHTS ? (
+            <Col xs={12}>
+              <b>{this.props.gameState.childGameState.house.name}</b> has to
+              destroy {this.props.gameState.childGameState.count} of their
+              Knights before the rest can be replaced with Footmen.
+            </Col>
+          ) : (
+            <Col xs={12}>Invalid CrowKillersStep!</Col>
+          ))}
+        {renderChildGameState(this.props, [
+          [SelectUnitsGameState, SelectUnitsComponent],
+          [TakeControlOfEnemyPortGameState, TakeControlOfEnemyPortComponent]
+        ])}
+      </>
+    );
+  }
 }
